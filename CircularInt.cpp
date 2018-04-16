084442252d79104ef9c809a78376d5129c1087f9
neorus <neorus666@gmail.com>
2018-04-16 10:06:02 +0000

    added exception

--- CircularInt.cpp
+++ CircularInt.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
+#include <string>
 #include "CircularInt.hpp"
 using namespace std;
 
-    CircularInt::CircularInt(int a, int b){
+
+    CircularInt::CircularInt(int min, int max){
         if(max<0 || max < min || min<0){
             cout << "Wrong values entered, defining default values 1-12";
-            max=12;
-            min=1;
+            this->max=12;
+            this->min=1;
             cur=1;
         } 
         else {
-            max = b;
-            min = a;
+            this->max = max;
+            this->min = min;
             cur = min;
         }
     }
         
-    CircularInt& CircularInt::operator=(const CircularInt cp){
+    CircularInt& CircularInt::operator= (const CircularInt& cp){
         this->max = cp.max;
         this->min = cp.min;
         this->cur = cp.cur;
@@ -28,7 +30,7 @@ using namespace std;
         return *this;
     }
         
-    CircularInt& CircularInt::operator+=(const CircularInt add){
+    CircularInt& CircularInt::operator+=(const CircularInt& add){
         cur += add.cur;
         cur = cur % max;
         if(cur < min)
@@ -44,7 +46,7 @@ using namespace std;
         return *this;
     }
         
-    CircularInt& CircularInt::operator-=(const CircularInt sub){
+    CircularInt& CircularInt::operator-=(const CircularInt& sub){
         cur -= sub.cur;
         cur = cur % max;
         if(cur < min)
@@ -60,7 +62,7 @@ using namespace std;
         return *this;
     }
         
-    CircularInt& CircularInt::operator*=(const CircularInt mul){
+    CircularInt& CircularInt::operator*=(const CircularInt& mul){
         cur *= mul.cur;
         cur = cur % max;
         if(cur < min)
@@ -76,7 +78,7 @@ using namespace std;
         return *this;
     }
         
-    CircularInt& CircularInt::operator/=(const CircularInt div){
+    CircularInt& CircularInt::operator/=(const CircularInt& div){
         cur /= div.cur;
         cur = cur % max;
         if(cur < min)
@@ -92,35 +94,60 @@ using namespace std;
         return *this;
     }
         
-    std::ostream& operator<<(ostream& os, const CircularInt& circ){
+    std::ostream& operator<<(ostream& os, CircularInt const & circ){
         return os << circ.cur;
     }
     
-    std::istream& operator>>(istream & is, const CircularInt& circ){     
-    }
+    //std::istream& operator>>(istream & is, const CircularInt& circ){     
+    //}
 
-    void CircularInt::operator+(CircularInt a){   
-    }
+    //void CircularInt::operator+(CircularInt a){   
+    //}
 
-
-    CircularInt& CircularInt::operator-(CircularInt){
-        return *this;
+    CircularInt operator - (int x, CircularInt const & obj){
+        CircularInt res {obj.min, obj.max};
+        res.cur = x;
+        res -= obj;
+        return res;
     }
     
-    CircularInt& CircularInt::operator-(int){
-        return *this;
+    CircularInt operator / ( CircularInt const & obj,int x){
+        CircularInt res {obj.min, obj.max};
+        res.cur = obj.cur;
+        if(res.cur%x==0)
+        res /= x;
+        else
+        throw string("There is no number x in {1,12} such that x*"+to_string(x)+"="+to_string(obj.cur)+" ");
+        return res;
+    }
+
+    CircularInt operator + (CircularInt const & a, CircularInt const & b){
+        CircularInt res {a.min, a.max};
+        res.cur = (a.cur + b.cur) % a.max;
+        return res;
+    }
+
+    CircularInt CircularInt::operator-(){
+       // cur = (max - cur) % max;
+       CircularInt res {min, max};
+       res.cur = cur;
+        res.cur -= max;
+        res.cur *= -1;
+        // if(cur<min)
+        // cur*=(-1);
+        return res;
     }
         
-    void CircularInt::operator*(int a){            
-    }
+    //void CircularInt::operator*(int a){            
+    //}
 
         CircularInt& CircularInt::operator++(int){
             cur++;
             return *this;
         }//
         
-        // CircularInt& CircularInt::operator--(int){    
-        //     return *this;
-        // }
+    // CircularInt& CircularInt::operator--(int){    
+    //     return *this;
+    // }
         
     CircularInt::~CircularInt(){};
--- CircularInt.hpp
+++ CircularInt.hpp
@@ -4,34 +4,35 @@ using namespace std;
 
 class CircularInt{
     private:
-       
-    public:
         int max;
         int min;
         int cur;
-        
+       
+    public:
         CircularInt(int, int);
-        CircularInt& operator =(CircularInt);
+        CircularInt& operator =(CircularInt const &);
         CircularInt& operator =(int);
-        CircularInt& operator+=(CircularInt);
+        CircularInt& operator+=(CircularInt const &);
         CircularInt& operator+=(int);
-        CircularInt& operator-=(CircularInt);
+        CircularInt& operator-=(CircularInt const &);
         CircularInt& operator-=(int);
-        CircularInt& operator*=(CircularInt);
+        CircularInt& operator*=(CircularInt const &);
         CircularInt& operator*=(int);
-        CircularInt& operator/=(CircularInt);
+        CircularInt& operator/=(CircularInt const &);
         CircularInt& operator/=(int);
+        friend CircularInt operator - (int, CircularInt const &);
+        friend CircularInt operator + (CircularInt const &, CircularInt const &);
+        friend CircularInt operator / ( CircularInt const &,int);
+        friend ostream& operator<<(ostream &, CircularInt const &);
+        friend istream& operator>>(istream & ,CircularInt const &);
         
-        void operator+(CircularInt);
-        void operator+(int);
-        CircularInt& operator-(CircularInt);
-        CircularInt& operator-(int);
-        void operator*(int);
-        void operator/(int);
+        //void operator+(CircularInt);
+        //void operator+(int);
+        //int operator- (int);
+        CircularInt operator- ();
+        //void operator*(int);
+        //void operator/(int);
         CircularInt& operator++(int);
-        void operator--(int);
+        //void operator--(int);
         ~CircularInt();
 };
-
-std::ostream& operator<<(ostream& , const CircularInt&);
-std::istream& operator>>(istream & , CircularInt&);
